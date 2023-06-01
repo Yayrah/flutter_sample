52b65c1ff36cb7dc51b14f0ddeba4e50932f83d7
yayra christabel <christabelyayra@yahoo.com>
2023-06-01 14:10:01 +0000

    v1.0.0.0

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const borderColor = Color(0xFF504A4A);
+const blue = Color(0xFF0A1D3A);
+const green = Color(0xFFC0E862);
+const white = Color(0xFFffffff);
+const grey = Color(0xFFd9d9d9);
+const black = Color(0xFF000000);
+
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:pet_life_gh/screens/Home/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     debugShowCheckedModeBanner: false,
+      home:  HomeScreen(),
+    );
+  }
+}
+
